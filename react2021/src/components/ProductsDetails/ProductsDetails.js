18ee064a959e1ec93e4801c9260244091bbd9983
Bazylek12 <a.kosmowski7@gmail.com>
2022-06-14 22:36:28 +0200

    cleaning

--- react2021/src/components/ProductsDetails/ProductsDetails.js
+++ react2021/src/components/ProductsDetails/ProductsDetails.js
@@ -1,60 +1,37 @@
 import React, { useState } from "react";
 import commonColumnsStyles from "../../common/styles/Columns.module.scss";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogTitle from "@mui/material/DialogTitle";
-import { Button } from "@mui/material";
-import { useSelector, useDispatch } from "react-redux";
-import axios from "axios";
+import { useSelector } from "react-redux";
 import { getSelectedProduct } from "../../redux/products/selectors";
 
 
 function ProductDetails() {
-  let navigate = useNavigate();
+    let navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1);
+    }
+
+    const productDetails = useSelector((store) => getSelectedProduct(store));
 
 
-  const goBackToAirportsList = () => {
-    navigate(-1);
-  }
-
-   const productDetails = useSelector((store) => getSelectedProduct(store));
-
-
-  return (
-    <>
-    
-      <ArrowBackIcon onClick={goBackToAirportsList} fontSize="large" />
-      <div className={commonColumnsStyles.App}>
-        <header className={commonColumnsStyles.AppHeader}>
-          <p>Products Details</p>
-          {productDetails && (
-            <>
-              <span>Nazwa: {productDetails.name}</span>
-              <span>Kategoria: {productDetails.category}</span>
-              <span>Jedzenie?: {productDetails.isFood ? ("Tak") : ("Nie")}</span>
-            </>
-           )} 
-        </header>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <ArrowBackIcon onClick={goBack} fontSize="large" />
+            <div className={commonColumnsStyles.App}>
+                <header className={commonColumnsStyles.AppHeader}>
+                    <p>Products Details</p>
+                    {productDetails && (
+                        <>
+                            <span>Nazwa: {productDetails.name}</span>
+                            <span>Kategoria: {productDetails.category}</span>
+                            <span>Jedzenie?: {productDetails.isFood ? ("Tak") : ("Nie")}</span>
+                        </>
+                    )}
+                </header>
+            </div>
+        </>
+    );
 }
 
-// const mapStateToProps = (store) => {
-//   return {
-//     getAirportById: (id) => getAirportByIdSelector(store, id),
-//   };
-// };
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     removeAirportById: (id) =>
-//       dispatch({ type: "REMOVE_AIRPORT_BY_ID", value: id }),
-//   };
-// };
-
 export default ProductDetails;
-// export default connect(mapStateToProps)AirportDetails;
